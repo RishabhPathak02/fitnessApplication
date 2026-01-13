a0d38e2ef2919825c804b9a1a12d135cd9dce672
Rishabh Pathak <rishabh06pathak@gmail.com>
2026-01-14 00:20:21 +0530

    feat : completed method based access

--- src/main/java/com/project/fitness/controller/SecurityController.java
+++ src/main/java/com/project/fitness/controller/SecurityController.java
@@ -1,10 +1,13 @@
 package com.project.fitness.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/hello")
     public String getMessage(){
         return "hello sir" ;
