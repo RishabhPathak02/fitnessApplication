210bac09f0e3dd7586fb18aac623618886f42046
Rishabh Pathak <rishabh06pathak@gmail.com>
2026-01-13 22:48:28 +0530

    feat : Completed UserDetails tutorial

--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,4 +20,21 @@ public class SecurityConfig {
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
+                .build() ;
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}password2")
+                .build() ;
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPassword")
+                .build() ;
+
+
+        return new InMemoryUserDetailsManager(user1 , user2 , admin);
+    }
+
 }
